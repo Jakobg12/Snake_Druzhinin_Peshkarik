6e9807a573764008d4672427e08cf4755faf2aae
paranoe <is-21-4-6@permaviat.ru>
2024-12-04 15:44:27 +0500

    Созданный метод добавления змеи игрока. Дружинин

--- Snake_Druzhinin_Peshkarik/Program.cs
+++ Snake_Druzhinin_Peshkarik/Program.cs
@@ -94,6 +94,23 @@ namespace Snake_Druzhinin_Peshkarik
                 Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n " + ex.Message);
             }
         }
+        public static int AddSnake()
+        {
+            ViewModelGames viewModelGamesPlayer = new ViewModelGames();
+            viewModelGamesPlayer.SnakesPlayers = new Snakes()
+            {
+                Points = new List<Snakes.Point>()
+                {
+                    new Snakes.Point() {X=30,Y=10},
+                    new Snakes.Point() {X=20,Y=10},
+                    new Snakes.Point() {X=10,Y=10},
+                },
+                direction = Snakes.Direction.Start
+            };
+            viewModelGamesPlayer.Points = new Snakes.Point(new Random().Next(10, 783), new Random().Next(10, 410));
+            viewModelGamesPlayer.Add(viewModelGamesPlayer);
+            return viewModelGames.FindIndex(x => x == viewModelGamesPlayer);
+        }
 
     }
 }
