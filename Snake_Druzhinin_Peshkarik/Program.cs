5f3fe9b889432a54e1d6c9b8767601499e9f01b8
paranoe <is-21-4-6@permaviat.ru>
2024-12-04 15:27:10 +0500

    Созданный метод Send. Дружинин

--- Snake_Druzhinin_Peshkarik/Program.cs
+++ Snake_Druzhinin_Peshkarik/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Snake_Druzhinin_Peshkarik
 {
@@ -16,6 +21,32 @@ namespace Snake_Druzhinin_Peshkarik
         public static int MaxSpeed = 15;
         static void Main(string[] args)
         {
+        }
+        private static void Send()
+        {
+            foreach (var User in remoteIPAddress)
+            {
+                UdpClient sender = new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint(
+                    IPAddress.Parse(User.IPAddress),
+                    int.Parse(User.Port));
+                try
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
+                    sender.Send(bytes, bytes.Length, endPoint);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Отправил данные пользователю: {User.IPAddress}:{User.Port}");
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n " + ex.Message);
+                }
+                finally
+                {
+                    sender.Close();
+                }
+            }
         }
     }
 }
